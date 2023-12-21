d95d21fb4751458788c855e2ae9819bb831c1fb9
popomon <popomonclub@gmail.com>
2023-12-21 22:59:25 +0900

    Added default files

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+	let score = 0;
+
+	function Question() {
+		const dom = document.createElement('div');
+	}
+
+	function Answer() {
+		const answer1 = document.createElement('div');
+		const answer2 = document.createElement('div');
+		const answer3 = document.createElement('div');
+		const answer4 = document.createElement('div');
+		const answer5 = document.createElement('div');
+	}
+})();
